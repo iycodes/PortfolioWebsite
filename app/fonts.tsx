05cb87fa40c8dc04623befb30da401761bb9f380
iycodes <iycodes@outlook.com>
2024-09-27 13:42:06 +0100

    all removed

--- app/fonts.tsx
+++
@@ -1,8 +0,0 @@
-import localFont from "next/font/local";
-
-export const rockitFont = localFont({
-  src: "..//public/fonts/rockit.ttf",
-});
-export const iceFont = localFont({
-  src: "..//public/fonts/ice.ttf",
-});
