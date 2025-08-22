bc608a96606c5a5680a3fa74f2179b71bce3161c
iycodes <iycodes@outlook.com>
2025-08-22 18:59:03 +0100

    added ecosystem config for pm2

--- static/8abS68_N_7RLjuiFducA5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-d0078775a707941c.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- static/chunks/app/layout-e0ff3ae3ef349a66.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5385:function(n,e,u){Promise.resolve().then(u.t.bind(u,159,23))},159:function(){}},function(n){n.O(0,[310,628,744],function(){return n(n.s=5385)}),_N_E=n.O()}]);
